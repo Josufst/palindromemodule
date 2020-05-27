d92afb879bad30a7ff4d65fde31f861533e79c01
Joost van der Linden <gjosuf@gmail.com>
2020-05-27 11:19:50 +0200

    NPM empty string test

--- index.js
+++ index.js
@@ -1,35 +1,37 @@
-// Reverses a string.
-function reverse(string) {
-  return Array.from(string).reverse().join("");
+module.exports = Phrase;
+
+// Adds `reverse` to all strings.
+String.prototype.reverse = function() {
+  return Array.from(this).reverse().join("");
 }
 
 // Defines a Phrase object.
 function Phrase(content) {
   this.content = content;
 
-  this.processor = function (string) {
-    return string.toLowerCase();
-  }
   // Returns content processed for palindrome testing.
   this.processedContent = function processedContent() {
-    return this.processor(this.content);
+      return this.letters().toLowerCase();
   }
+
+  // Returns the letters in the content.
+  // For example:
+  //   new Phrase("Hello, world!").letters() === "Helloworld"
+  this.letters = function letters() {
+    const lettersRegEx = /[a-z]/gi;
+    return Array.from(this.content).filter(c => c.match(lettersRegEx)).join("");
+  }
+
+
+  // this.letters = function letters() {
+  //   return (this.content.match(/[a-z]/gi) || []).join("");
+  // }
 
   // Returns true if the phrase is a palindrome, false otherwise.
   this.palindrome = function palindrome() {
-    return this.processedContent() === reverse(this.processedContent());
+    if (this.content) {
+    return this.processedContent() === this.processedContent().reverse();
+  } else {
+    return false;
   }
 }
-
-// Defines a TranslatedPhrase object.
-function TranslatedPhrase(content, translation) {
-  this.content = content;
-  this.translation = translation;
-
-  // Returns translation processed for palindrome testing.
-  this.processedContent = function processedContent() {
-    return this.processor(this.translation);
-  }
-}
-
-TranslatedPhrase.prototype = new Phrase();
